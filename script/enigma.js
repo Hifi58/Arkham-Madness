51cc551a81ea55f06b5767d38ea851108614a8b0
Hifi58 <gillet.tony@gmail.com>
2021-05-31 10:50:35 +0200

    page de garde ok

--- script/enigma.js
+++ script/enigma.js
@@ -1,8 +1,18 @@
-const check = document.getElementById("start");
-check.addEventListener("start", function () {
-  let animate = document.getElementById("test").value;
+document.querySelector(".push").addEventListener("click", play, false);
+document.querySelector(".titre").className ="titre";
+document.querySelector(".a").className ="a";
+document.querySelector(".b").className ="b";
+document.querySelector(".c").className ="c";
+document.querySelector(".d").className ="d";
+document.querySelector(".e").className ="e";
+document.querySelector(".f").className ="f";
+document.querySelector(".g").className ="g";
+document.querySelector(".h").className ="h";
 
-  if(())
-});
-
-//en construction
+function play(){
+  window.requestAnimationFrame(function(time){
+    window.requestAnimationFrame(function(time){
+      document.querySelector("push").className="titre push";
+    });
+  });
+} 
